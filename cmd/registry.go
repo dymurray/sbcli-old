892807f62fb5756c7e3e307a246e3d4a77dd7d6f
Dylan Murray <dymurray@redhat.com>
2018-05-15 12:03:02 -0400

    Add registry remove command

--- cmd/registry.go
+++ cmd/registry.go
@@ -10,6 +10,7 @@ import (
 
 var registryConfig registries.Config
 var whitelist string
+var removeName string
 
 var registryCmd = &cobra.Command{
 	Use:   "registry",
@@ -26,6 +27,15 @@ var registryAddCmd = &cobra.Command{
 	},
 }
 
+var registryRemoveCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Remove a registry adapter",
+	Long:  `Remove a registry adapter from stored configuration`,
+	Run: func(cmd *cobra.Command, args []string) {
+		removeRegistry()
+	},
+}
+
 var registryListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List the configured registry adapters",
@@ -37,6 +47,7 @@ var registryListCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(registryCmd)
+	// Registry Add Flags
 	registryAddCmd.Flags().StringVar(&registryConfig.Type, "type", "dockerhub", "Type of registry adapter to add")
 	registryAddCmd.Flags().StringVar(&registryConfig.Org, "org", "ansibleplaybookbundle", "Type of registry adapter to add")
 	registryAddCmd.Flags().StringVar(&registryConfig.URL, "url", "docker.io", "URL of registry adapter to add")
@@ -44,8 +55,13 @@ func init() {
 	registryAddCmd.Flags().StringVar(&whitelist, "whitelist", ".*-apb$", "Whitelist for configuration of registry adapter")
 	registryConfig.WhiteList = append(registryConfig.WhiteList, whitelist)
 
+	//Registry Remove Flags
+	registryRemoveCmd.Flags().StringVar(&removeName, "name", "", "Name of registry adapter to remove")
+	registryRemoveCmd.MarkFlagRequired("name")
+
 	registryCmd.AddCommand(registryAddCmd)
 	registryCmd.AddCommand(registryListCmd)
+	registryCmd.AddCommand(registryRemoveCmd)
 }
 
 func updateCachedRegistries(registries []registries.Config) error {
@@ -55,16 +71,17 @@ func updateCachedRegistries(registries []registries.Config) error {
 }
 
 func addRegistry() {
-	//	reg, err := registries.NewRegistry(registryConfig, "ansible-service-broker")
-	//	if err != nil {
-	//		fmt.Printf("Error creating new registry adapter: %v", err)
-	//		return
-	//	}
 	var regList []registries.Config
 	err := viper.UnmarshalKey("Registries", &regList)
 	if err != nil {
 		fmt.Println("Error unmarshalling config: ", err)
 		return
+	}
+	for _, reg := range regList {
+		if reg.Name == registryConfig.Name {
+			fmt.Printf("Error adding registry [%v], found registry with conflicting name [%v]\n", registryConfig.Name, reg.Name)
+			return
+		}
 	}
 
 	regList = append(regList, registryConfig)
@@ -76,17 +93,31 @@ func listRegistries() {
 	var regList []registries.Config
 	err := viper.UnmarshalKey("Registries", &regList)
 	if err != nil {
-		fmt.Println("Error unmarshalling config: ", err)
+		fmt.Printf("Error unmarshalling config: %v", err)
 		return
 	}
 	if len(regList) > 0 {
 		fmt.Println("Found registries already in config:")
 		for _, r := range regList {
-			fmt.Printf("name: %v - type: %v - URL: %v\n", r.Name, r.Type, r.URL)
+			fmt.Printf("name: %v - type: %v - organization: %v - URL: %v\n", r.Name, r.Type, r.Org, r.URL)
 		}
 	} else {
 		fmt.Println("Found no registries in configuration. Try `sbcli registry add`.")
 	}
 	return
+}
 
+func removeRegistry() {
+	var regList []registries.Config
+	var newRegList []registries.Config
+	err := viper.UnmarshalKey("Registries", &regList)
+	if err != nil {
+		fmt.Printf("Error unmarshalling config: %v", err)
+	}
+	for i, r := range regList {
+		if r.Name == removeName {
+			newRegList = append(regList[:i], regList[i+1:]...)
+		}
+	}
+	updateCachedRegistries(newRegList)
 }
